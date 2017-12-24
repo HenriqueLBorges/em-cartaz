9649894051bb1ce13d221c09312fb3583ba632f4
Henrique Borges <henriquelb96@gmail.com>
2017-12-24 09:12:21 -0200

    remove spaces and \n from results

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -65,21 +65,6 @@ function getMovies(res) {
                         elenco.push($(this).text());
                     });
 
-                    google(titulo, function (err, res) {
-                        if (err) console.error(err)
-
-                        for (var i = 0; i < res.links.length; ++i) {
-                            var link = res.links[i];
-                            console.log(link.title + ' - ' + link.href)
-                            console.log(link.description + "\n")
-                        }
-
-                        if (nextCounter < 4) {
-                            nextCounter += 1
-                            if (res.next) res.next()
-                        }
-                    })
-
                     if (titulo !== "") {
                         filmes.push(
                             {
